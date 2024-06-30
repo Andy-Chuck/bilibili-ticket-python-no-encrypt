240dbbad1e8d1572e2f957c35b0f2fa8d947bebb
Andy Chuck <i@andychuck.cn>
2024-06-30 16:55:07 +0800

    First

---
+++ cli.py
@@ -0,0 +1,115 @@
+import atexit
+import shutil
+import sys
+import threading
+
+from loguru import logger
+
+from interface import ProductCli, SettingCli, UserCli
+from util import Captcha, Config, Notice, Request, Task
+
+
+def cleanup_meipass() -> None:
+    if hasattr(sys, "_MEIPASS"):
+        meipass_path = sys._MEIPASS  # type: ignore
+        try:
+            shutil.rmtree(meipass_path)
+            print(f"正在清理 {meipass_path}")
+        except Exception as e:
+            print(f"清理失败 {meipass_path}: {e}")
+
+
+atexit.register(cleanup_meipass)
+
+if __name__ == "__main__":
+    # 丢锅
+    print(
+        """
+|=====================================================================
+|
+|  欢迎使用 https://github.com/biliticket/transition-ticket
+|  本程序仅供学习交流, 不得用于商业用途
+|  使用本程序进行违法操作产生的法律责任由操作者自行承担
+|  对本程序进行二次开发/分发时请注意遵守GPL-3.0开源协议
+|  本脚本仅适用于蹲回流票, 我们反对将其用于抢票
+|  黄牛４０００＋
+|
+|=====================================================================
+|
+|  交互: 上下 键盘↑↓键, 多选 空格, 确认 回车
+|
+|=====================================================================
+"""
+    )
+
+    # 日志
+    logger.add(
+        "log/{time}.log",
+        colorize=False,
+        enqueue=True,
+        encoding="utf-8",
+        # 日志保留份数
+        retention=3,
+        # 调试
+        backtrace=True,
+        diagnose=True,
+    )
+
+    # 初始化
+    # 用户数据文件
+    userData = Config(dir="user")
+    productData = Config(dir="product")
+    settingData = Config(dir="setting")
+    # 验证
+    cap = Captcha()
+
+    # 检测配置文件情况
+    userList = userData.List()
+    productList = productData.List()
+    settingList = settingData.List()
+
+    # 读取配置
+    userConfig = UserCli(conf=userData).Select(selects=userList) if userList != [] else UserCli(conf=userData).Generate()
+    productConfig = ProductCli(conf=productData).Select(selects=productList) if productList != [] else ProductCli(conf=productData).Generate()
+    settingConfig = SettingCli(conf=settingData).Select(selects=settingList) if settingList != [] else SettingCli(conf=settingData).Generate()
+
+    net = Request(
+        cookie=userConfig["cookie"],
+        header=userConfig["header"],
+        timeout=settingConfig["request"]["timeout"],
+        proxy=settingConfig["request"]["proxy"],
+    )
+
+    job = Task(
+        net=net,
+        cap=cap,
+        sleep=settingConfig["request"]["sleep"],
+        projectId=productConfig["projectId"],
+        screenId=productConfig["screenId"],
+        skuId=productConfig["skuId"],
+        buyer=userConfig["buyer"],
+    )
+
+    # job.DrawFSM()
+
+    # 任务流
+    if job.Run():
+        notice = Notice(title="抢票", message="下单成功! 请在十分钟内支付")
+        mode = settingConfig["notice"]
+        logger.success("【抢票】下单成功! 请在十分钟内支付")
+
+        # 多线程通知
+        noticeThread = []
+        t1 = threading.Thread(target=notice.Message)
+        t2 = threading.Thread(target=notice.Sound)
+        t3 = threading.Thread(target=notice.PushPlus, args=(mode["plusPush"],))
+
+        if mode["system"]:
+            noticeThread.append(t1)
+        if mode["sound"]:
+            noticeThread.append(t2)
+        if mode["wechat"]:
+            noticeThread.append(t3)
+
+        for t in noticeThread:
+            t.start()
---
+++ util/Task/__init__.py
@@ -0,0 +1,308 @@
+import logging
+from time import sleep, time
+
+from loguru import logger
+from transitions import Machine, State
+
+from util import Bilibili, Captcha, Request
+
+
+class Task:
+    """
+    状态机
+    """
+
+    @logger.catch
+    def __init__(
+        self,
+        net: Request,
+        cap: Captcha,
+        sleep: int,
+        projectId: int,
+        screenId: int,
+        skuId: int,
+        buyer: dict,
+    ):
+        """
+        初始化
+
+        net: 网络实例
+        cap: 验证码实例
+        sleep: 任务间请求间隔时间
+        projectId: 项目ID
+        screenId: 场次ID
+        skuId: 商品ID
+        buyer: 购买者信息
+        """
+
+        self.net = net
+        self.cap = cap
+        self.sleep = sleep
+        self.api = Bilibili(net=self.net, projectId=projectId, screenId=screenId, skuId=skuId, buyer=buyer)
+
+        self.states = [
+            State(name="开始"),
+            State(name="等待开票", on_enter="WaitAvailableAction"),
+            State(name="获取Token", on_enter="QueryTokenAction"),
+            State(name="验证码", on_enter="RiskProcessAction"),
+            State(name="等待余票", on_enter="QueryTicketAction"),
+            State(name="创建订单", on_enter="CreateOrderAction"),
+            State(name="创建订单状态", on_enter="CreateStatusAction"),
+            State(name="完成"),
+        ]
+
+        # from transitions.extensions import GraphMachine
+        self.machine = Machine(
+            model=self,
+            states=self.states,
+            initial="开始",
+            # show_state_attributes=True,
+        )
+
+        self.machine.add_transition(
+            trigger="Next",
+            source="开始",
+            dest="等待开票",
+        )
+
+        self.machine.add_transition(
+            trigger="WaitAvailable",
+            source="等待开票",
+            dest="获取Token",
+        )
+
+        # 0-成功, 1-验证码, 2-失败
+        self.machine.add_transition(
+            trigger="QueryToken",
+            source="获取Token",
+            dest="创建订单",
+            conditions=lambda: self.queryTokenResult == 0,
+        )
+        self.machine.add_transition(
+            trigger="QueryToken",
+            source="获取Token",
+            dest="验证码",
+            conditions=lambda: self.queryTokenResult == 1,
+        )
+        self.machine.add_transition(
+            trigger="QueryToken",
+            source="获取Token",
+            dest="获取Token",
+            conditions=lambda: self.queryTokenResult == 2,
+        )
+
+        # True-成功, False-失败
+        self.machine.add_transition(
+            trigger="RiskProcess",
+            source="验证码",
+            dest="获取Token",
+            conditions=lambda: self.riskProcessResult is True,
+        )
+        self.machine.add_transition(
+            trigger="RiskProcess",
+            source="验证码",
+            dest="验证码",
+            conditions=lambda: self.riskProcessResult is False,
+        )
+
+        # True-成功, False-失败
+        self.machine.add_transition(
+            trigger="QueryTicket",
+            source="等待余票",
+            dest="创建订单",
+            conditions=lambda: self.queryTicketResult is True,
+        )
+        self.machine.add_transition(
+            trigger="QueryTicket",
+            source="等待余票",
+            dest="等待余票",
+            conditions=lambda: self.queryTicketResult is False,
+        )
+
+        # 0-成功, 1-刷新, 2-等待, 3-失败
+        self.machine.add_transition(
+            trigger="CreateOrder",
+            source="创建订单",
+            dest="创建订单状态",
+            conditions=lambda: self.createOrderResult == 0,
+        )
+        self.machine.add_transition(
+            trigger="CreateOrder",
+            source="创建订单",
+            dest="获取Token",
+            conditions=lambda: self.createOrderResult == 1,
+        )
+        self.machine.add_transition(
+            trigger="CreateOrder",
+            source="创建订单",
+            dest="等待余票",
+            conditions=lambda: self.createOrderResult == 2,
+        )
+        self.machine.add_transition(
+            trigger="CreateOrder",
+            source="创建订单",
+            dest="创建订单",
+            conditions=lambda: self.createOrderResult == 3,
+        )
+
+        # True-成功, False-失败
+        self.machine.add_transition(
+            trigger="CreateStatus",
+            source="创建订单状态",
+            dest="完成",
+            conditions=lambda: self.createStatusResult is True,
+        )
+        self.machine.add_transition(
+            trigger="CreateStatus",
+            source="创建订单状态",
+            dest="创建订单",
+            conditions=lambda: self.createStatusResult is False,
+        )
+
+        # 上次刷新时间
+        self.lastTime = time()
+
+        # 关闭Transitions自带日志
+        logging.getLogger("transitions").setLevel(logging.CRITICAL)
+
+    @logger.catch
+    def WaitAvailableAction(self) -> None:
+        """
+        等待开票
+        """
+        start_time = self.api.GetSaleStartTime()
+        countdown = start_time - int(time())
+        logger.info(f"【等待开票】本机时间已校准!")
+
+        if countdown > 0:
+            logger.warning("【等待开票】请确保本机时间是北京时间, 服务器用户尤其要注意!")
+
+            while countdown > 0:
+                countdown = start_time - int(time())
+
+                if countdown >= 3600:
+                    logger.info(f"【等待开票】需要等待 {countdown/60:.1f} 分钟")
+                    sleep(600)
+                    countdown -= 600
+
+                elif 3600 > countdown >= 600:
+                    logger.info(f"【等待开票】需要等待 {countdown/60:.1f} 分钟")
+                    sleep(60)
+                    countdown -= 60
+
+                elif 600 > countdown >= 60:
+                    logger.info(f"【等待开票】准备开票! 需要等待 {countdown/60:.1f} 分钟")
+                    sleep(5)
+                    countdown -= 5
+
+                elif 60 > countdown > 0:
+                    logger.info(f"【等待开票】即将开票! 需要等待 {countdown} 秒")
+                    sleep(1)
+                    countdown -= 1
+
+            if countdown == 0:
+                logger.info("【等待开票】等待结束! 开始抢票")
+
+        else:
+            logger.info("【等待开票】已开票! 开始进入抢票模式")
+
+    @logger.catch
+    def QueryTokenAction(self) -> None:
+        """
+        获取Token
+
+        返回值: 0-成功, 1-风控, 2-未开票, 3-未知
+        """
+        self.queryTokenResult = self.api.QueryToken()
+        self.lastTime = time()
+
+        # 顺路
+        if self.queryTokenResult == 0:
+            self.api.QueryAmount()
+
+        # 防风控
+        else:
+            sleep(self.sleep)
+
+    @logger.catch
+    def RiskProcessAction(self) -> None:
+        """
+        验证码
+
+        返回值: 0-成功, 1-取消验证, 2-失败
+        """
+        match self.api.RiskInfo():
+            case 0:
+                challenge = self.api.GetRiskChallenge()
+                validate = self.cap.Geetest(challenge)
+                self.riskProcessResult = self.api.RiskValidate(validate)
+                self.riskProcessResult = True
+            case 1:
+                self.riskProcessResult = True
+            case 2:
+                self.riskProcessResult = False
+
+    @logger.catch
+    def QueryTicketAction(self) -> None:
+        """
+        等待余票
+
+        返回值: True-成功, False-失败
+        """
+        self.queryTicketResult = self.api.QueryAmount()
+
+        if not self.queryTicketResult:
+            # 防风控
+            sleep(self.sleep)
+
+    @logger.catch
+    def CreateOrderAction(self) -> None:
+        """
+        创建订单
+
+        返回值: 0-成功, 1-刷新, 2-等待, 3-失败
+        """
+        self.createOrderResult = self.api.CreateOrder()
+
+        if self.createOrderResult != 0:
+            # 防风控
+            sleep(self.sleep)
+
+    @logger.catch
+    def CreateStatusAction(self) -> None:
+        """
+        创建订单状态
+
+        返回值: True-成功, False-失败
+        """
+        self.createStatusResult = self.api.GetOrderStatus() if self.api.CreateOrderStatus() else False
+
+    @logger.catch
+    def DrawFSM(self) -> None:
+        """
+        状态机图输出
+        """
+        self.machine.get_graph().draw("./assest/fsm.png", prog="dot")
+
+    @logger.catch
+    def Run(self) -> bool:
+        """
+        任务流
+        """
+        job = {
+            "开始": "Next",
+            "等待开票": "WaitAvailable",
+            "获取Token": "QueryToken",
+            "验证码": "RiskProcess",
+            "等待余票": "QueryTicket",
+            "创建订单": "CreateOrder",
+            "创建订单状态": "CreateStatus",
+        }
+
+        while self.state != "完成":  # type: ignore
+            sleep(0.15)
+            self.trigger(job[self.state])  # type: ignore
+            if time() >= self.lastTime + 9 * 60:
+                logger.info("【刷新Token】已经9分钟没刷新Token了! 开始刷新")
+                self.to_获取Token()  # type: ignore
+        return True
